646b45a86e14d0796c8b526ffef43a1fdd3eca34
Swayam Badhe <swayampbadhe@gmail.com>
2024-01-22 22:29:08 +0530

    feat: Add userVerification middleware

--- server/routes/AuthRoute.js
+++ server/routes/AuthRoute.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const { SignUp, Login } = require('../Controllers/AuthController');
+const { userVerification } = require('../middleware/AuthMiddleware');
 
 router.post('/signup', SignUp);
 router.post('/login', Login);
+router.post('/', userVerification);
 
 module.exports = router;
