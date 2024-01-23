520ba94d1b8cfdc235e53b842c1f49d9ced8d3d9
Swayam Badhe <swayampbadhe@gmail.com>
2024-01-23 13:02:22 +0530

    feat: Add endpoint to retrieve documents

--- server/routes/AuthRoute.js
+++ server/routes/AuthRoute.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const { SignUp, Login } = require('../Controllers/AuthController');
+const { getDocuments } = require('../Controllers/documentController');
 const { userVerification } = require('../middleware/AuthMiddleware');
 
 router.post('/signup', SignUp);
 router.post('/login', Login);
 router.post('/', userVerification);
+router.get('/documents', getDocuments);
 
 module.exports = router;
