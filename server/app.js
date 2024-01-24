ab4c762b7832c823fdabe3011746f1af14063656
Swayam Badhe <swayampbadhe@gmail.com>
2024-01-24 13:05:26 +0530

    refactor

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ app.use(
     allowedHeaders: ['Origin', 'Content-Type', 'Accept'],
   })
 );
-
+app.use(express.static('public'));
 app.use((req, res, next) => {
   console.log('Received request:', req.method, req.url);
   next();
